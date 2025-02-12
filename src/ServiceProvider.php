faca031d1389991c63e117dfefc2f1d4fa9094c9
rooy <rooyzhang@users.noreply.github.com>
2025-02-12 09:11:58 +0800

    1

---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+namespace Huozi\LaravelWechatNotification;
+
+use Illuminate\Support\ServiceProvider AS LaravelServiceProvider;
+use Illuminate\Support\Facades\Notification;
+use Huozi\LaravelWechatNotification\Channels\WechatTemplateChannel;
+
+
+class ServiceProvider extends LaravelServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $channels = [
+            'official_account',
+            'mini_program',
+            'open_official_account',
+            'open_mini_program',
+            'work',
+            'open_work'
+        ];
+
+        foreach ($channels as $channel) {
+            Notification::extend($channel, function ($app) use ($channel) {
+                return new WechatTemplateChannel($channel);
+            });
+        }
+    }
+}
---
+++ src/WechatMessage.php
@@ -0,0 +1,37 @@
+<?php
+namespace Huozi\LaravelWechatNotification;
+
+use Overtrue\LaravelWeChat\Facade;
+use Huozi\LaravelWechatNotification\Messages\OfficateAccountTemplateMessage;
+use Huozi\LaravelWechatNotification\Messages\MiniProgramTemplateMessage;
+use Huozi\LaravelWechatNotification\Messages\WechatWorkMessage;
+
+class WechatMessage
+{
+
+    public static function officialAccount($name = '')
+    {
+        return new OfficateAccountTemplateMessage(Facade::officialAccount($name));
+    }
+
+    public static function miniProgram($name = '')
+    {
+        return new MiniProgramTemplateMessage(Facade::miniProgram($name));
+    }
+
+    public static function openFlatform($name = '')
+    {
+        return new WechatPlatform(Facade::openPlatform($name));
+    }
+
+    public static function work($name = '')
+    {
+        return new WechatWorkMessage(Facade::work($name)->message);
+    }
+
+    public static function openWork($name = '')
+    {
+        return new WechatOpenWork($name ? app('wechat.open_work.'.$name) : app('wechat.open_work'));
+    }
+}
+
